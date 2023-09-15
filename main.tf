b72dc69d252e4a988a95ad98ddc1e72b1103d5e1
aung ko htet <aungkohtet717@gmail.com>
2023-09-15 23:32:00 +0000

    #9 add randon terraform provider

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length    = 16
+  special   = false
+}
+
+output "randon_bucket_name" {
+  value = random_string.bucket_name.result
+}
 
