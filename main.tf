b99bf03424a515d44a2a2d8e16074e9a03dff438
aung ko htet <aungkohtet717@gmail.com>
2023-09-18 15:29:23 +0000

    #12 migrate local state to terraform cloud using terraform cloud  block

--- main.tf
+++ main.tf
@@ -1,22 +1,54 @@
 terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "aungkohtet"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  cloud {
+    organization = "aungkohtet"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region
+  access_key = "AKIA3PP3KODU5TCVN667"  # Replace with your AWS access key
+  secret_key = "/Ms363YwaSxJdjChUuqHNY+Oter2zV94K4NQs+/S"  # Replace with your AWS secret key
+}
 provider "random" {
   # Configuration options
 }
 
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  length    = 16
-  special   = false
+  lower = true
+  upper = false
+  length   = 32
+  special  = false
 }
 
-output "randon_bucket_name" {
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = random_string.bucket_name.result
+}
+
+output "random_bucket_name" {
   value = random_string.bucket_name.result
 }
-
