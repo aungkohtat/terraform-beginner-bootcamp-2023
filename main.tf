b10b1dd276bd28bb6e4f8422f8e6a3b14cfdd693
aung ko htet <aungkohtet717@gmail.com>
2023-10-04 09:43:26 +0000

    #25 refractor s3 bucket into the new terrahouse_aws module

--- main.tf
+++ main.tf
@@ -1,18 +1,22 @@
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-#resource "random_string" "bucket_name" {
-#  lower = true
-#  upper = false
-#  length   = 32
-#  special  = false
+terraform {
+# backend "remote" {
+#   hostname = "app.terraform.io"
+#   organization = "aungkohtet"
+#
+#   workspaces {
+#     name = "terra-house-1"
+#   }
+# }
+#cloud {
+#  organization = "aungkohtet"
+#  workspaces {
+#    name = "terra-house-1"
+#  }
 #}
+}
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
-  }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
