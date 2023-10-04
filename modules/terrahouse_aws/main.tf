e81356ca22eb0b16ccb94fb82ad620e42a0f21c3
Aung Ko Htet <aungkohtet717@gmail.com>
2023-10-04 16:15:48 +0630

    #25 refractor s3 bucket into the new terrahouse_aws module (#26)

---
+++ modules/terrahouse_aws/main.tf
@@ -0,0 +1,18 @@
+terraform{
+    required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = var.bucket_name
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
