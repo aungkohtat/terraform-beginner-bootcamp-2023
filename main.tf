4f82dd03a63c3f98188e8be50dde1f48d89e903a
Aung Ko Htet <aungkohtet717@gmail.com>
2023-10-05 13:11:43 +0630

    #27 enable static website hosting, uploaded index and error file via terraform (#28)

--- main.tf
+++ main.tf
@@ -1,22 +1,25 @@
 terraform {
-# backend "remote" {
-#   hostname = "app.terraform.io"
-#   organization = "aungkohtet"
-#
-#   workspaces {
-#     name = "terra-house-1"
-#   }
-# }
-#cloud {
-#  organization = "aungkohtet"
-#  workspaces {
-#    name = "terra-house-1"
-#  }
-#}
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "aungkohtet"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  #cloud {
+  #  organization = "aungkohtet"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+
 }
 
 module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "bucket_name" {
-  description = "bucket name for our static website hosting"
+  description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.bucket_name
+}
+
+output "s3_website_endpoint" {
+  description = "S3 Static Website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
 }
