1dde5c9c660189a20553f636808e385745080a77
aung ko htet <aungkohtet717@gmail.com>
2023-10-09 04:09:47 +0000

    #35 upload assets using a for_each

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -34,6 +34,19 @@ resource "aws_s3_object" "index_html" {
     ignore_changes = [ etag ]
   }
 }
+resource "aws_s3_object" "upload_assets"{
+  for_each = fileset(var.assets_path,"*.{jpg,png,gif}")
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "assets/${each.key}"
+  source = "${var.assets_path/${each.key}"
+  etag = filemd5("${var.assets_path/${each.key}")
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [ etag ]
+
+  }
+}
+
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 resource "aws_s3_object" "error_html" {
@@ -72,3 +85,4 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "terraform_data" "content_version" {
   input = var.content_version
 }
+
