60c6aefdde01e378082c63a6ff4e95937e020b4d
aung ko htet <aungkohtet717@gmail.com>
2023-10-11 08:38:46 +0000

    wip setting up the skeleton for creating a custom terraform provider 2

---
+++ go.mod
@@ -0,0 +1,3 @@
+module terraform-provider-terratowns_v1.0.0
+
+go 1.21.1
--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -1,21 +1,16 @@
-// package main: Declares the package name. 
+// package main: Declares the package name.
 // The main package is special in Go, it's where the execution of the program starts.
 package main
 
 // fmt is short format, it contains functions for formatted I/O.
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"net/http"
-	"log"
 	"fmt"
-	"github.com/google/uuid"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
-// func main(): Defines the main function, the entry point of the app. 
+
+// func main(): Defines the main function, the entry point of the app.
 // When you run the program, it starts executing from this function.
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
@@ -23,42 +18,44 @@ func main() {
 	})
 	// Format.PrintLine
 	// Prints to standard output
-	fmt.Println("Hello, world!")
+	fmt.Println("Hello, world")
 }
 
-type Config struct {
-	Endpoint string
-	Token string
-	UserUuid string
-}
-
-// in golang, a titlecase function will get exported.
+// In golang, a titlecase function will get exported.
 func Provider() *schema.Provider {
 	var p *schema.Provider
 	p = &schema.Provider{
-		ResourcesMap:  map[string]*schema.Resource{
-			"terratowns_home": Resource(),
-		},
-		DataSourcesMap:  map[string]*schema.Resource{
-
-		},
+		ResourcesMap:   map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{},
 		Schema: map[string]*schema.Schema{
 			"endpoint": {
-				Type: schema.TypeString,
-				Required: true,
-				Description: "The endpoint for hte external service",
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The endpoint for the external service",
 			},
 			"token": {
-				Type: schema.TypeString,
-				Sensitive: true, // make the token as sensitive to hide it the logs
-				Required: true,
-				Description: "Bearer token for authorization",
+				Type:        schema.TypeString,
+				Sensitive:   true, // make the token as sensitive to hide in the logs.
+				Required:    true,
+				Description: "Bearer token for authorisation",
 			},
 			"user_uuid": {
-				Type: schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Required:    true,
 				Description: "UUID for configuration",
-				ValidateFunc: validateUUID,
+				// ValidateFunc: validateUUID,
 			},
 		},
 	}
+	// p.ConfigureContextFunc = providerConfigure(p)
+	return p
+}
+
+// func validateUUID(v interface{}, k string) (ws []string, errors []error) {
+// 	log.Print('validateUUID:start')
+// 	value := v.(string)
+// 	if _, err = uuid.Parse(value); err != nil {
+// 		errors = append(error, fmt.Errorf("invalid UUID format"))
+// 	}
+// 	log.Print('validateUUID:end')
+// }
