735ad2a64b86049338385468e24964995af5e9ff
aung ko htet <aungkohtet717@gmail.com>
2023-10-13 08:26:00 +0000

    #53 test our terra home on terratowns test town

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -2,8 +2,9 @@
 resource "aws_s3_bucket" "website_bucket" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
-
+  #bucket = var.bucket_name
+  #we want to assign a random bucket name
+  #bucket = var.bucket_name
   tags = {
     UserUuid = var.user_uuid
   }
--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-#output "bucket_name" {
-#  description = "Bucket name for our static website hosting"
-#  value = module.terrahouse_aws.bucket_name
-#}
-#
-#output "s3_website_endpoint" {
-#  description = "S3 Static Website hosting endpoint"
-#  value = module.terrahouse_aws.website_endpoint
-#}
-#output "cloudfront_url" {
-#  description = "cloudfront distribution domain name"
-#  value = module.terrahouse_aws.cloudfront_url
-#}
+output "bucket_name" {
+  description = "Bucket name for our static website hosting"
+  value = module.terrahouse_aws.bucket_name
+}
+
+output "s3_website_endpoint" {
+  description = "S3 Static Website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
+output "cloudfront_url" {
+  description = "cloudfront distribution domain name"
+  value = module.terrahouse_aws.cloudfront_url
+}
