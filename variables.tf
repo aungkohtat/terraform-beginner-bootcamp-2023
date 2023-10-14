8528b20573b659c63ae7040359cfe3638aed7eb4
Aung Ko Htet <aungkohtet717@gmail.com>
2023-10-14 06:57:08 +0630

    #55 fix issue with provider,setup to different towns (#56)

--- variables.tf
+++ variables.tf
@@ -7,24 +7,16 @@ variable "terratowns_access_token" {
 variable "teacherseat_user_uuid" {
  type = string
 }
-variable "bucket_name" {
- type = string
+variable "arcanum" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
 
-variable "index_html_filepath" {
-  type = string
-}
-
-variable "error_html_filepath" {
-  type = string
-}
-
-variable "content_version" {
-  type        = number
-
-}
-
-variable "assets_path" {
-  description = "Path to assets"
-  type = string
+variable "kyarpauk" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
